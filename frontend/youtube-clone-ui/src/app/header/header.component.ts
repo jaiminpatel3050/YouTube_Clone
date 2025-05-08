cf7ea1520900042a734eed7a796639a0eb14506e
Jaimin Patel <jaiminpatel3050@gmail.com>
2025-05-08 12:15:29 -0400

    logout  changes

--- frontend/youtube-clone-ui/src/app/header/header.component.ts
+++ frontend/youtube-clone-ui/src/app/header/header.component.ts
@@ -1,30 +1,38 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 import { OidcSecurityService } from 'angular-auth-oidc-client';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+  styleUrls: ['./header.component.css'],
 })
 export class HeaderComponent {
-
   isAuthenticated: boolean = false;
 
-  constructor(private oidcSecurityService: OidcSecurityService){}
+  constructor(
+    private oidcSecurityService: OidcSecurityService,
+    private router: Router
+  ) {}
 
-  ngOnInit():void{
-      this.oidcSecurityService.isAuthenticated$.subscribe(({isAuthenticated})=>{
+  ngOnInit(): void {
+    this.oidcSecurityService.isAuthenticated$.subscribe(
+      ({ isAuthenticated }) => {
         this.isAuthenticated = isAuthenticated;
-      })
+      }
+    );
   }
 
-  login(){
+  login() {
     this.oidcSecurityService.authorize();
-    console.log("login called");
+    console.log('login called');
   }
 
   logOff() {
+    console.log('logOff called');
     this.oidcSecurityService.logoffAndRevokeTokens();
     this.oidcSecurityService.logoffLocal();
+    this.router.navigateByUrl('/featured');
+    console.log('is still authenticated?', this.isAuthenticated);
   }
 }
